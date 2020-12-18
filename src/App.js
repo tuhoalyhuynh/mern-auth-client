dfea7ab7ff68fb315986541a6d4868f2d6ca56cc
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-18 15:15:34 -0500

    [App] Create PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
 import Welcome from './components/Welcome'
 import Navbar from './components/Navbar'
 import './App.css';
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const user = localStorage.getItem('jwtToken');
+  return <Route { ...rest } render={(props) => {
+    return user ? <Component { ...rest } { ...props }/> : <Redirect to="/login"/>
+  }}
+}
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Welcome />
     </div>
   );
