fce5ea4bc500ea322110a342a67e7f675509f072
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-18 12:17:34 -0500

    [components] Add About and Footer components

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is about us...</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia, expedita tempore perferendis quae iusto ipsa, corporis necessitatibus eligendi pariatur eius iste quis deserunt cum ea non. Quisquam, saepe magni.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-dark">
+            <div className="container text-centeer">
+                <span className="text-muted">@2020 Built by Tu Hoa Huynh (during COVID)</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
