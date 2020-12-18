1c98460d43f3ce32e1e9de0c81c05120584eacb6
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-18 12:53:16 -0500

    [Navbar] Add auth to links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Welcome />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,25 @@ const Navbar = (props) => {
                             <NavLink className="nav-link" exact to="/about">About</NavLink>
                         </li>
                     </ul>
+                    {
+                        props.isAuth ?
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/profile">Profile</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <span onClick={props.handleLogout} className="nav-link logout-link">Logout</span>
+                            </li>
+                        </ul>
+                        : <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/signup">Create Account</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/login">Login</NavLink>
+                            </li>
+                        </ul>
+                    }
                 </div>
             </div>
         </nav>
